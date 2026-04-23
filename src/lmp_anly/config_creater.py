a3d14fbd052367ff3b9aad6d14b146374ae864ec
hujian <hyd201704@outlook.com>
2026-04-23 16:10:57 +0800

    feat(config_creater): add more energy plot template to simplify usage. more energy plot template has been added for green hand's out of box usage, but the user-defined variable still need manual addition.

--- src/lmp_anly/config_creater.py
+++ src/lmp_anly/config_creater.py
@@ -27,17 +27,27 @@ def ensure_default_config():
                 "lines.linewidth": 0.35
             },
             "line_element": {
-                "density": asdict(FigDict("Density", r"$\mathrm{g/cm^3}$", "Density", "density")),
+                "density": asdict(FigDict("Density", r"Density ($\mathrm{g/cm^3}$)", "Density", "density")),
+                "press": asdict(FigDict("Press", "Pressure (atm.)", "Press", "press")),
+                "volume": asdict(FigDict("Volume", r"Volume ($\mathrm{Å^{3}}$)", "Volume", "volume")),
                 "temp": asdict(FigDict("Temp", "Temperature(°C)", "Temperature", "temp")),
-                "rmsd": asdict(FigDict("v_rmsd", "RMSD", "RMSD", "rmsd")),
+                "rmsd": asdict(FigDict("v_rmsd", "RMSD (Å)", "RMSD", "rmsd")),
                 "total_energy": asdict(FigDict("TotEng", "Energy (kcal/mol)", "Total Energy", "energy")),
                 "kin_energy": asdict(FigDict("KinEng", "Energy (kcal/mol)", "Kinetic Energy", "energy")),
                 "pot_energy": asdict(FigDict("PotEng", "Energy (kcal/mol)", "Potential Energy", "energy")),
-                "mol_energy": asdict(FigDict("E_mol", "Energy (kcal/mol)", "Molecular Energy", "energy")),
                 "pair_energy": asdict(FigDict("E_pair", "Energy (kcal/mol)", "Pairwise Energy", "energy")),
-                "dipole_x": asdict(FigDict("v_dipole_x", "Dipole Moment", "Dipole Moment in X", "dipole")),
-                "dipole_y": asdict(FigDict("v_dipole_y", "Dipole Moment", "Dipole Moment in Y", "dipole")),
-                "dipole_z": asdict(FigDict("v_dipole_z", "Dipole Moment", "Dipole Moment in Z", "dipole")),
+                "coul_energy": asdict(FigDict("E_coul", "Energy (kcal/mol)", "Coulombic Energy", "energy")),
+                "vdwl_energy": asdict(FigDict("E_vdwl", "Energy (kcal/mol)", "van der Waals Pairwise Energy", "energy")),
+                "long_energy": asdict(FigDict("E_long", "Energy (kcal/mol)", "Long-range Energy", "energy")),
+                "mol_energy": asdict(FigDict("E_mol", "Energy (kcal/mol)", "Molecular Energy", "energy")),
+                "bond_energy": asdict(FigDict("E_bond", "Energy (kcal/mol)", "Bond Energy", "energy")),
+                "angle_energy": asdict(FigDict("E_angle", "Energy (kcal/mol)", "Angle Energy", "energy")),
+                "dihed_energy": asdict(FigDict("E_dihed", "Energy (kcal/mol)", "Dihedral Energy", "energy")),
+                "imp_energy": asdict(FigDict("E_impro", "Energy (kcal/mol)", "Improper Energy", "energy")),
+                "enthalpy": asdict(FigDict("Enthalpy", "Energy (kcal/mol)", "Enthalpy", "energy")),
+                "dipole_x": asdict(FigDict("v_dipole_x", r"Dipole Moment ($\mathrm{eV \cdot Å}$)", "Dipole Moment in X", "dipole")),
+                "dipole_y": asdict(FigDict("v_dipole_y", r"Dipole Moment ($\mathrm{eV \cdot Å}$)", "Dipole Moment in Y", "dipole")),
+                "dipole_z": asdict(FigDict("v_dipole_z", r"Dipole Moment ($\mathrm{eV \cdot Å}$)", "Dipole Moment in Z", "dipole")),
             }
         }
         with open(config_file, "w", encoding="utf-8") as f:
